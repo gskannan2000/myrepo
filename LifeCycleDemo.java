54bb8f2aa8d16ed15634356a1fa1238c0aa88052
kannan <gskannan2000@gmail.com>
2024-11-18 11:57:41 +0530

    testing file

---
+++ LifeCycleDemo.java
@@ -0,0 +1,45 @@
+package com.cg.unittest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LifeCycleDemo {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("@BeforeAll");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("@AfterAll");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("@BeforeEach");
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("@AfterEach");
+	}
+
+	@Test
+	@Disabled
+	@DisplayName("Test 1")
+	void test1() {
+		System.out.println("test 1");
+	}
+
+	@Test
+	@DisplayName("Test 2")
+	void test2() {
+		System.out.println("test 2");
+	}
+}
